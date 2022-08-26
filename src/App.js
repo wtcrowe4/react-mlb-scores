b9993fb86d06b14d4a679b1994c56fc701d3c708
Thomas Crowe <wcrowe@g.clemson.edu>
2022-08-25 22:59:26 -0400

    Moved MLB scores data into component

--- src/App.js
+++ src/App.js
@@ -1,70 +1,23 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-// import { getScores } from './helper';
-//import ScoreDisplay from './components/ScoreDisplay';
+//import { useEffect, useState } from 'react';
+import ScoreDisplayMLB from './components/ScoreDisplayMLB';
 
 
-function App() {
-  const [scoresArray, setScoresArray] = useState([]);
-  
-  
-  useEffect(() => {
-    const RapidAPIKey = process.env.REACT_APP_RAPIDAPIKEY
-    const options = {
-      method: 'GET',
-      headers: {
-      'X-RapidAPI-Key': RapidAPIKey,
-        'X-RapidAPI-Host': 'odds.p.rapidapi.com'
-      }
-    };
-    fetch('https://odds.p.rapidapi.com/v4/sports/baseball_mlb/scores?daysFrom=3', options)
-    .then(response => response.json())
-    .then(response => setScoresArray(response))
-    .catch(err => console.error(err))
-  }
-  , []);
-  
 
-  if(scoresArray.length > 0 ) {
-    // const sport = scores[0].sport_title
-    // const homeTeam = score.scores[0].name;
-    // const awayTeam = score.scores[1].name;
-    const displayScores = scoresArray.map(scoreArray => {
-      const game = scoreArray.scores
-      if(game !== null) {
-        return (
-          <div className='singleGameDiv' key={scoreArray.id}>
-            <h4>{game[0].name}: {game[0].score} </h4>
-            <p>at</p>
-            <h4>{game[1].name}: {game[1].score}</h4>
-            
-          </div>
-        )
-      } else {
-        return (
-          <div className='singleGameDiv' key={scoreArray.id}>
-            <h4>Not Started</h4>
-            <p>{scoreArray.away_team} @ {scoreArray.home_team}</p>
-          </div>
-        )
-      }
-    })
+function App(props) {
+   
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>MLB Scores</h1>
+        <h1>Sports Scores</h1>
       </header>
       <div className="container">
-        {/* <ScoreDisplay data={scores}/> */}
-        {displayScores}
-      
+        <ScoreDisplayMLB />
       </div>
     </div>
   );
-  } else {
-    return <div>Loading...</div>
-  }
+
 }
 
 export default App;
