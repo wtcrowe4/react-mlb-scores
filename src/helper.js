3671577429673b3a235023b08c483667d138ceb3
Thomas Crowe <wcrowe@g.clemson.edu>
2022-08-29 22:36:18 -0400

    Installed axios, started on helper logic.

--- src/helper.js
+++ src/helper.js
@@ -1,16 +1,41 @@
 import { useState, useEffect } from 'react';
 //create a helper file to store fetch functions
-//create variables to determine odds or scores
+//need to install axios, import axios from 'axios'
 
 
 
 const helper = () => {
-	// const state = useState()
+	// const state = getState()
 	// const sport = state.sport
+	// const daysFrom = state.daysFrom
 	// const oddsURL = `https://odds.p.rapidapi.com/v4/sports/${sport}/odds?daysFrom=3`  
 	// const scoresURL = `https://odds.p.rapidapi.com/v4/sports/${sport}/scores?daysFrom=3`
-	
-	
+	// const RapidAPIKey = process.env.REACT_APP_RAPIDAPIKEY 
+	//using axios to fetch data from the API
+	// const API = {
+	// 	method: 'GET',
+	// 	headers: {	
+	// 		'X-RapidAPI-Key': RapidAPIKey,
+	// 		'X-RapidAPI-Host': 'odds.p.rapidapi.com'
+	// 	},
+	//	params: {
+	//		daysFrom: 'daysFrom'
+	//  },
+	//  url: oddsURL || scoresURL
+	// }
+	// const fetchData = async () => {
+	// 	const response = await axios(API)
+	// 	return response.data
+	// }
+	// const data = await fetchData()
+	// return data
+	// }
+	// axios.request(API).then(response => {
+	// 	console.log(response)
+	// }).catch(err => {	
+	// 	console.log(err)
+	// }
+
 
 
 }
@@ -18,7 +43,7 @@ const helper = () => {
 // const options = {
 // 	method: 'GET',
 // 	headers: {
-// 		'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+// 		'X-RapidAPI-Key': RapidAPIKey,
 // 		'X-RapidAPI-Host': 'odds.p.rapidapi.com'
 // 	}
 // };
